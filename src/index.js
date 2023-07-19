91d6690090d1eb6287a882288bb804aa359c2562
ekamaul <ekamaher@gmail.com>
2023-07-19 23:15:15 +0700

    update mouseover header dan lain2

--- src/index.js
+++ src/index.js
@@ -1,37 +1,75 @@
 const header1 = document.getElementById('header1')
 const rove = document.getElementById('rove')
+const opa = document.getElementById('opa')
+const lokasi = document.getElementById('lokasi')
 
 
 
-const lokasi = ["Jakarta", "Banten", "Maluku"]
+const lokasilist = ["Jakarta", "Banten", "Maluku", "Cirebon", "Teluk Benoa", "Bangka"]
 
 
 
 function mouseinout (elementid, change) {
-    elementid.addEventListener("mouseover", ()=>{
+    elementid.addEventListener("mouseenter", ()=>{
         elementid.classList.add(change)
     })
-    elementid.addEventListener("mouseout", ()=>{
+    elementid.addEventListener("mouseleave", ()=>{
         elementid.classList.remove(change)
     })
 }
 
-function showsubhead (triger_el, change_el,) {
-    document.getElementById(triger_el).addEventListener("mouseover", ()=> {
-        document.getElementById(change_el).classList.remove("hidden")
+function showsubhead (triger_el1, change_el1, triger_el2, change_el2 ) {
+    document.getElementById(triger_el1).addEventListener("mouseenter", ()=> {
+        document.getElementById(change_el1).classList.replace("hidden", "block");
+        document.getElementById(change_el2).classList.replace("block", "hidden" )
+
     });
-    document.getElementById(change_el).addEventListener("mouseover", ()=> {
+
+    document.getElementById(triger_el2).addEventListener("mouseenter", ()=> {
+        document.getElementById(change_el2).classList.replace("hidden", "block");
+        document.getElementById(change_el1).classList.replace("block", "hidden" )
+
+    });
+    document.getElementById(change_el1).addEventListener("mouseenter", ()=> {
         header1.classList.add("bg-green-900")
     });
-    document.getElementById(change_el).addEventListener("mouseleave", ()=> {
-        document.getElementById(change_el).classList.add("hidden");
-        header1.classList.remove("bg-green-900")
+    document.getElementById(change_el2).addEventListener("mouseenter", ()=> {
+        header1.classList.add("bg-green-900")
+    });
 
+    document.getElementById(change_el1).addEventListener("mouseleave", ()=> {
+        document.getElementById(change_el1).classList.add("hidden");
+        header1.classList.remove("bg-green-900")
     })
+
+    document.getElementById(change_el2).addEventListener("mouseleave", ()=> {
+        document.getElementById(change_el2).classList.add("hidden");
+        header1.classList.remove("bg-green-900")
+    })
+
+    
 }
 
 mouseinout(rove, "text-green-400")
 mouseinout(header1, "bg-green-900")
 
-showsubhead("itemhead1", "subhead1")
-showsubhead("itemhead2", "subhead2")
+showsubhead("itemhead1", "subhead1", "itemhead2", "subhead2")
+
+window.onscroll = function() {changehead()};
+
+function changehead() {
+  if (document.documentElement.scrollTop > 20) {
+    header1.classList.add("bg-green-900");
+    opa.classList.replace("opacity-70", "opacity-80");
+    
+  } else {
+    header1.classList.remove("bg-green-900")
+    opa.classList.replace("opacity-80", "opacity-70");
+  }
+}
+
+
+
+lokasi.innerHTML = lokasilist[100]
+
+
